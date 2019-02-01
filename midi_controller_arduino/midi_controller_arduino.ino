57e76cf652ae2b12184eaa47e635fa8dcbe9f0c3
Thomas Kercheval <spacekattpoispin@gmail.com>
2019-01-31 18:44:48 -0800

    Change MIDIUSB to MIDI Library
    Since my Arduino does not support native USB, this other
    MIDI library works. MIDI Library sends the MIDI over serial,
    then I use Hairless MIDI to translate the serial terminal input
    into MIDI messages.

--- midi_controller_arduino/midi_controller_arduino.ino
+++ midi_controller_arduino/midi_controller_arduino.ino
@@ -1,11 +1,14 @@
-#include "MIDIUSB.h"
+#include "MIDI.h"
+
+MIDI_CREATE_DEFAULT_INSTANCE();
 
 const int CONTROLLER_00_PIN = 0;
 
 int val_00 = 0;
 
 void setup() {
-  Serial.begin(9600);
+  MIDI.begin(1);
+  Serial.begin(115200);
 }
 
 void loop() {
@@ -13,19 +16,11 @@ void loop() {
   int tmp = analogRead(CONTROLLER_00_PIN) / 8;
 
   if (tmp != val_00) {
-    MIDImessage(176, 1, tmp);
+
+    // Cont #, Cont val, channel
+    MIDI.sendControlChange(7, tmp, 1);
     val_00 = tmp;
   }
 
   delay(10);
-}
-
-void noteOn(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
-  MidiUSB.sendMIDI(noteOn);
-}
-
-void noteOff(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
-  MidiUSB.sendMIDI(noteOff);
 }
